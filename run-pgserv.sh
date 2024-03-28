d88eea4dc01d5f645eff145263977f86cbf9d192
borhez <getlink32@gmail.com>
2024-03-29 01:44:02 +0300

    add *.config

--- run-pgserv.sh
+++ run-pgserv.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-NET1=postgres1
-USER='POSTGRES_USER=postgres'
-PASSWD='POSTGRES_PASSWORD=postgres'
-SOURCE='/home/sshield/languages/psql/postgres.git/src/launch-in-container.sh'
-DEST_SCRYPT='/pg/initdb.sh'
-MOUNT="${SOURCE}:${DEST_SCRYPT}"
-SERVER_DIR='/pg/server-data'
-HOSTNAME=pgserver
+source vars.config
 
-docker network create $NET1
-docker run -d -h $HOSTNAME -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET1 -P postgres:16.2-alpine3.19
+docker network create $NET
+docker run -d -h $HOSTNAME -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET -P postgres:16.2-alpine3.19
 ID=$(docker ps | head -n2 | awk 'NR>1 {print $1}')
 docker exec -u root $ID mkdir $SERVER_DIR
 docker exec -u root $ID chown postgres:postgres $SERVER_DIR
