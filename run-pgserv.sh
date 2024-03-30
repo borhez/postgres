ceb8a6e02fb0aa96b28394baf788cda36dae926b
borhez <getlink32@gmail.com>
2024-03-30 18:31:13 +0300

    edit files

--- run-pgserv.sh
+++ run-pgserv.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-
 source vars.config
 
 docker network create $NET
-docker run -d -h $HOSTNAME -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET -P $OS_IMAGE
+docker run -d -h ${HOSTNAME1} -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET -P $PG_IMAGE
 ID=$(docker ps | head -n2 | awk 'NR>1 {print $1}')
-docker exec -u root $ID mkdir $SERVER_DIR
-docker exec -u root $ID chown postgres:postgres $SERVER_DIR
-docker exec -u postgres $ID bash ${DEST_SCRYPT} $SERVER_DIR
+
+#для контейнера, в котором не запущен сервер-postgres, надо раскомментировать след. 3 строки, т.к.
+#в официальном контейнере-postgres, уже запущен сервер.
+#docker exec -u root $ID mkdir $SERVER_DIR
+#docker exec -u root $ID chown postgres:postgres $SERVER_DIR
+#docker exec -u postgres $ID bash ${DEST_SCRYPT} $SERVER_DIR
 
 docker exec -it -u postgres $ID bash
 
