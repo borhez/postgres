36214b16e95e19145150675c11f0162335d3a6ea
borhez <109803145+borhez@users.noreply.github.com>
2024-03-30 17:58:09 +0300

    Update run-pgserv.sh

--- run-pgserv.sh
+++ run-pgserv.sh
@@ -3,7 +3,7 @@
 source vars.config
 
 docker network create $NET
-docker run -d -h $HOSTNAME -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET -P postgres:16.2-alpine3.19
+docker run -d -h $HOSTNAME -u postgres --rm -e $USER -e $PASSWD -v $MOUNT --network $NET -P $OS_IMAGE
 ID=$(docker ps | head -n2 | awk 'NR>1 {print $1}')
 docker exec -u root $ID mkdir $SERVER_DIR
 docker exec -u root $ID chown postgres:postgres $SERVER_DIR
